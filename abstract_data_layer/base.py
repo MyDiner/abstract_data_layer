fe53595ec3219cb91e0b7796afb5e8f504c85122
Andrea Bozzelli <74464364+bozzelliandrea@users.noreply.github.com>
2024-01-14 21:59:05 +0100

    pagination data models

--- abstract_data_layer/base.py
+++ abstract_data_layer/base.py
@@ -14,14 +14,17 @@ and then choose `flask` as template.
 """
 from abc import abstractmethod
 from datetime import datetime
-from typing import TypeVar, Generic, Protocol
+from enum import Enum
+from typing import TypeVar, Generic, Protocol, List
 
 ID = TypeVar('ID')
 T = TypeVar('T')
 
+
 class Entity(Protocol):
     createDate: datetime
     updateDate: datetime
+
 
 class CRUD(Generic[T, ID]):
 
@@ -40,3 +43,50 @@ class CRUD(Generic[T, ID]):
     @abstractmethod
     def update(self, obj: T) -> T:
         pass
+
+
+class SortOrder(Enum):
+    ASC = 1
+    DESC = 2
+
+
+class Sorting(Generic[T]):
+    def __init__(self, field: str, order: SortOrder):
+        self.order = order
+        if not hasattr(T, field):
+            raise KeyError(f"Field {field} doesn't exists in object")
+
+    @staticmethod
+    def asc(field: str) -> 'Sorting':
+        return Sorting(field, SortOrder.ASC)
+
+    @staticmethod
+    def desc(field: str) -> 'Sorting':
+        return Sorting(field, SortOrder.DESC)
+
+
+class PageRequest(Generic[T]):
+
+    def __init__(self, page: int = 0, size: int = 20, query: T = None, sorting: SortOrder[T] = None):
+        self.page = page
+        self.size = size
+        self.query = query
+        self.sorting = sorting
+
+    @staticmethod
+    def on(page: int) -> 'PageRequest':
+        return PageRequest(page)
+
+
+class PageResponse(Generic[T]):
+
+    def __init__(self, elements: List[T] = None,
+                 page: int = 0,
+                 page_size: int = 0,
+                 total_pages: int = 0,
+                 total_size: int = 0):
+        self.elements = elements if elements is not None else []
+        self.page = page
+        self.page_size = page_size
+        self.total_pages = total_pages
+        self.total_size = total_size
